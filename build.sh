b717705da537e350bd01b531de41520de591665b
Andrea055 <vivanco11.ac@gmail.com>
2022-08-06 18:33:28 +0200

    add shortcut

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+desktop=$(xdg-user-dir DESKTOP)
 mkdir ~/wol/Guitar_Pro_8
 cp ~/wol/Downloads/Guitar_Pro_8/icon.png ~/wol/Downloads/Guitar_Pro_8/startup.sh ~/wol/Guitar_Pro_8
+cp ~/wol/Downloads/Guitar_Pro_8/guitarpro.desktop $desktop
 echo "Downloading Guitar Pro 8..."
 wget https://download-fr-3.guitar-pro.com/gp8/stable/guitar-pro-8-setup.exe
 echo "Preparing Wineprefix enviroment..."
