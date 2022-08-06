aa8e4af4ef7d414039a7a83ba8489c8a4656197c
Andrea055 <vivanco11.ac@gmail.com>
2022-08-06 18:35:56 +0200

    change to curl

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ mkdir ~/wol/Guitar_Pro_8
 cp ~/wol/Downloads/Guitar_Pro_8/icon.png ~/wol/Downloads/Guitar_Pro_8/startup.sh ~/wol/Guitar_Pro_8
 cp ~/wol/Downloads/Guitar_Pro_8/guitarpro.desktop $desktop
 echo "Downloading Guitar Pro 8..."
-wget https://download-fr-3.guitar-pro.com/gp8/stable/guitar-pro-8-setup.exe
+curl https://download-fr-3.guitar-pro.com/gp8/stable/guitar-pro-8-setup.exe --output guitar-pro-8-setup.exe
 echo "Preparing Wineprefix enviroment..."
 WINEARCH=win64 WINEPREFIX=~/wol/Guitar_Pro_8/ winetricks win7
 WINEARCH=win64 WINEPREFIX=~/wol/Guitar_Pro_8/ winetricks corefonts
